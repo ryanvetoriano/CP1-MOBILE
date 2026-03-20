3a9592dbf69420c6f3b87bd145d97e75d0d2a72b
ryanvetoriano <vetorianosilva@gmail.com>
2026-03-20 18:27:36 -0300

    feat: Configurando app.js.

--- cp1-mobile/App.js
+++ cp1-mobile/App.js
@@ -1,18 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FormComponent from './components/FormComponent';
 
 export default function App() {
   return (
-    
-    <View style={styles.container}>
-      <SafeAreaView>
-        <Text>Open up App.js to start working on your </Text>
-        <FormComponent/>
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FormComponent />
+      <StatusBar style="auto" />
+    </SafeAreaView>
   );
 }
 
@@ -20,7 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
